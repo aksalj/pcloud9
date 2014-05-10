951bac6bf9ee15dfc62540eb8ec91b63677f6448
Salama A.B <aksalj@aksalj.me>
2014-05-10 12:42:57 +0300

    Add Cloud9 source

--- app/static/js/cloud9.js
+++ app/static/js/cloud9.js
@@ -2,17 +2,36 @@
 
 var fs = require('fs');
 var path = require('path');
-var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+
+var log = function (msg) {
+    console.log(msg);
+    $("#log").append("<span>" + msg + "<span></span><br>");
+};
+
+var startCloud9 = function (callback) {
+
+    var c9 = spawn('node', ['cloud9/server.js']);
+
+    c9.stdout.on('data', function (data) {
+        log('stdout: ' + data);
+    });
+
+    c9.stderr.on('data', function (data) {
+        log('stderr: ' + data);
+    });
+
+    c9.on('close', function (code) {
+        log('child process exited with code ' + code);
+    });
+
+};
 
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    exec("dir", function (err, stdout, stderr) {
-        if (err !== null) {
-            console.log('exec error: ' + err);
-            return callback(err);
-        }
-
+    var files = fs.readdirSync(process.cwd());
+    files.forEach( function(item) {
+        log(item);
     });
-
 });
